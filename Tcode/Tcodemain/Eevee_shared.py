fa8a3db96e95d45fe0c8ba5da3fd4f118715ba53
jasmine <jasminetbarker@outlook.co.nz>
2026-04-14 11:39:38 +1200

    Bug fixing

--- Tcode/Tcodemain/Eevee_shared.py
+++ Tcode/Tcodemain/Eevee_shared.py
@@ -1,7 +1,9 @@
 #these are the common functions shared among the other sections of Tabitha code.
-def zigzagbotleft(toprow, botrow, leftcol, rightcol, mask):
-    numcol = rightcol - leftcol
-    numrow = botrow - toprow
+def zigzagbotleft(mask, toprow, botrow, leftcol, rightcol):
+    print("XX", toprow, botrow, leftcol, rightcol)
+    numcol = rightcol - leftcol - 1
+    numrow = botrow - toprow - 1
+    print("huasfhaj", numcol, numrow, botrow)
     for startpix in range (numrow, 0, -1):
         r = startpix
         c = leftcol
@@ -10,5 +12,41 @@ def zigzagbotleft(toprow, botrow, leftcol, rightcol, mask):
                 print("Found white bottom left ", r, c)
                 return r, c
             else:
+                print(r, c)
                 r = r + 1
                 c = c + 1
+
+
+
+# print(numrow)
+# print(numcol)
+# colorpixel = mask[500, 500] 
+# #This grabs the top left point on a garment.
+# for currentrow in range (100, numrow-100, 1):
+#     for currentcol in range (100, numcol-100, 1):
+#         currentcolour = mask[currentrow, currentcol]   
+#         if currentcolour == 255:
+#             topwhite = currentrow
+#             print(topwhite, currentcolour)
+#             stopnowbool = True
+#             break
+#     if stopnowbool == True:
+#             break
+    
+# stopnowbool = False
+
+# for currentrow in range (numrow-100, 100, -1):
+#     for currentcol in range (numcol-100, 100, -1):
+#         currentcolour = mask[currentrow, currentcol]
+#         # find corners of garment
+#         if currentcolour == 255:
+#             bottomwhite = currentrow
+#             print(bottomwhite, currentcolour)
+#             stopnowbool = True
+#             break
+#     if stopnowbool == True:
+#             break
+
+# heightinpixels = bottomwhite - topwhite
+# heightincm = heightinpixels/30
+# print(heightincm)
--- Tcode/Tcodemain/snorlax.py
+++ Tcode/Tcodemain/snorlax.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import math
-from .Eevee_shared import zigzagbotleft 
+from Eevee_shared import zigzagbotleft 
 # photo height = 1140, photo width = 1035
 # pixels per cm = 1140/38 = 30 for height and 1035/34.5 = 30
 # MAT_W_CM = 34.5 MAT_H_CM = 38.0 these are the numbers from the rectification
@@ -23,7 +23,7 @@ color_img = cv2.resize(color_img, (mask.shape[1], mask.shape[0]))
 mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 colored_masked = cv2.bitwise_and(color_img, mask_3ch)
 
-cv2.imshow("Colored Masked", colored_masked)
+# cv2.imshow("Colored Masked", colored_masked)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # print(mask.shape)
@@ -31,36 +31,9 @@ stopnowbool = False
 pixelspercm = 30
 numrow = mask.shape[0]
 numcol = mask.shape[1]
-zigzagbotleft(toprow, botrow, leftcol, rightcol, mask)
-print(numrow)
-print(numcol)
-colorpixel = mask[500, 500] 
-#This grabs the top left point on a garment.
-for currentrow in range (100, numrow-100, 1):
-    for currentcol in range (100, numcol-100, 1):
-        currentcolour = mask[currentrow, currentcol]   
-        if currentcolour == 255:
-            topwhite = currentrow
-            print(topwhite, currentcolour)
-            stopnowbool = True
-            break
-    if stopnowbool == True:
-            break
-    
-stopnowbool = False
-
-for currentrow in range (numrow-100, 100, -1):
-    for currentcol in range (numcol-100, 100, -1):
-        currentcolour = mask[currentrow, currentcol]
-        # find corners of garment
-        if currentcolour == 255:
-            bottomwhite = currentrow
-            print(bottomwhite, currentcolour)
-            stopnowbool = True
-            break
-    if stopnowbool == True:
-            break
-
-heightinpixels = bottomwhite - topwhite
-heightincm = heightinpixels/30
-print(heightincm)
+toprow = 0
+botrow = mask.shape[0]
+leftcol = 0
+rightcol = mask.shape[1]
+print(toprow, botrow, leftcol, rightcol)
+botleft = zigzagbotleft(mask, toprow, botrow, leftcol, rightcol)
