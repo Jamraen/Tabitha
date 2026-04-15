cc650a560d25b63bd83f519208bba6052b93fcb8
jasmine <jasminetbarker@outlook.co.nz>
2026-04-15 14:37:50 +1200

    Made code to remove aruco markers from corner of mask, created a showimg function so I could visualise what the code was doing, and started on a way to find the corners of a garment. TDT, Marking the found corner and finding the other three corners

--- Tcode/Tcodemain/Eevee_shared.py
+++ Tcode/Tcodemain/Eevee_shared.py
@@ -1,22 +1,61 @@
+import cv2
+import numpy as np
+import math
 #these are the common functions shared among the other sections of Tabitha code.
 def zigzagbotleft(mask, toprow, botrow, leftcol, rightcol):
-    print("XX", toprow, botrow, leftcol, rightcol)
-    numcol = rightcol - leftcol - 1
-    numrow = botrow - toprow - 1
-    print("huasfhaj", numcol, numrow, botrow)
+    print("toprow", toprow, "botrow",botrow, "leftcol",leftcol, "rightcol",rightcol)
+    numcol = rightcol - 1
+    numrow = botrow - 1
+    botrow = botrow - 1
+    rightcol = rightcol - 1
+    print("numcol", numcol, "numrow",numrow, "botrow",botrow)
     for startpix in range (numrow, 0, -1):
         r = startpix
         c = leftcol
+        print(r,c)
         while r > toprow and c<= numcol:
             if mask[r, c] >= 250:
                 print("Found white bottom left ", r, c)
                 return r, c
             else:
                 print(r, c)
-                r = r + 1
+                r = r - 1
                 c = c + 1
 
-
+def showmaskimage(mask, title="rename this window"):
+    resized = cv2.resize(mask, dsize=None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow(title, resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+def removearucomask(mask, toprow, botrow, leftcol, rightcol):
+    arucosize = 110
+    botrow = botrow -1
+    rightcol = rightcol - 1
+    #Below is top left
+    for row in range (0, arucosize, 1):
+        for col in range (0, arucosize, 1):
+            if mask[row, col] >= 250:
+                mask[row, col] = 0
+    #Above is top left
+    #Below is bot left
+    for row in range (botrow, botrow -arucosize, -1):
+        for col in range (0, arucosize, 1):
+            if mask[row, col] >= 250:
+                mask[row, col] = 0
+    #Above is bot left
+    #Below it top right
+    for row in range (0, arucosize, 1):
+        for col in range (rightcol, rightcol - arucosize, -1):
+            if mask[row, col] >= 250:
+                mask[row, col] = 0
+    #Above it top right
+    #Below is bottom right
+    for row in range (botrow, botrow - arucosize, -1):
+        for col in range (rightcol, rightcol - arucosize, -1):
+            if mask[row, col] >= 250:
+                mask[row, col] = 0
+    #above is bottom right.
+    return mask
 
 # print(numrow)
 # print(numcol)
--- Tcode/Tcodemain/snorlax.py
+++ Tcode/Tcodemain/snorlax.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import math
 from Eevee_shared import zigzagbotleft 
+from Eevee_shared import removearucomask
+from Eevee_shared import showmaskimage
 # photo height = 1140, photo width = 1035
 # pixels per cm = 1140/38 = 30 for height and 1035/34.5 = 30
 # MAT_W_CM = 34.5 MAT_H_CM = 38.0 these are the numbers from the rectification
@@ -24,8 +26,8 @@ mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 colored_masked = cv2.bitwise_and(color_img, mask_3ch)
 
 # cv2.imshow("Colored Masked", colored_masked)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 # print(mask.shape)
 stopnowbool = False
 pixelspercm = 30
@@ -36,4 +38,6 @@ botrow = mask.shape[0]
 leftcol = 0
 rightcol = mask.shape[1]
 print(toprow, botrow, leftcol, rightcol)
+arucoremove = removearucomask(mask, toprow, botrow, leftcol, rightcol)
+showimagemask = showmaskimage(mask, title="rename this window")
 botleft = zigzagbotleft(mask, toprow, botrow, leftcol, rightcol)
